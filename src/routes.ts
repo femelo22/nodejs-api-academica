93c34ee12fa1f704515ffe3c228bdcde558acc65
Luiz Fernando M Gonçalves <56324956+femelo22@users.noreply.github.com>
2022-09-03 23:55:09 -0300

    feat: criando repo de rooms

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
+import { RoomController } from './controller/RoomController';
 import { SubjectController } from './controller/SubjectController';
 
 const routes = Router();
 
 routes.post('/subjects', new SubjectController().create)
+routes.post('/rooms', new RoomController().create)
 
 
 export default routes
